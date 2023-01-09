021db2210a276a5c76a9fb43d4d4e36701af6d75
shivermetim <93898982+shivermetim@users.noreply.github.com>
2023-01-09 12:52:45 +1100

    bukhara

---
+++ lite-xl-bukhara-23.01.09/session.lua
@@ -0,0 +1,1 @@
+return {recents={[1]="D:\\OneDrive"}, window={[1]=1552,[2]=903,[3]=155,[4]=54,["n"]=4}, window_mode="normal", previous_find={}, previous_replace={}}
---
+++ lite-xl-bukhara-23.01.09/user_settings.lua
@@ -0,0 +1,54 @@
+return {
+  ["config"] = {
+    ["borderless"] = true,
+    ["code_font"] = {
+      ["fonts"] = {
+        [1] = {
+          ["name"] = "NotoMono NF Book",
+          ["path"] = "C:\\Users\\shive\\AppData\\Local\\Microsoft\\Windows\\Fonts\\Noto Mono Nerd Font Complete Mono Windows Compatible.ttf"
+        }
+      },
+      ["options"] = {
+        ["antialiasing"] = "subpixel",
+        ["bold"] = false,
+        ["hinting"] = "slight",
+        ["italic"] = false,
+        ["size"] = 15,
+        ["smoothing"] = false,
+        ["strikethrough"] = false,
+        ["underline"] = false
+      }
+    },
+    ["enabled_plugins"] = {
+      ["drawwhitespace"] = true
+    },
+    ["file_size_limit"] = 8,
+    ["font"] = {
+      ["fonts"] = {
+        [1] = {
+          ["name"] = "NotoSans NF Regular",
+          ["path"] = "C:\\Users\\shive\\AppData\\Local\\Microsoft\\Windows\\Fonts\\Noto Sans Regular Nerd Font Complete Windows Compatible.ttf"
+        }
+      },
+      ["options"] = {
+        ["antialiasing"] = "subpixel",
+        ["bold"] = false,
+        ["hinting"] = "full",
+        ["italic"] = false,
+        ["size"] = 15,
+        ["smoothing"] = false,
+        ["strikethrough"] = false,
+        ["underline"] = false
+      }
+    },
+    ["indent_size"] = 5,
+    ["line_height"] = 1.5,
+    ["plugins"] = {
+      ["lineguide"] = {
+        ["enabled"] = true
+      }
+    },
+    ["tab_type"] = "hard",
+    ["theme"] = "gruvbox_dark"
+  }
+}
